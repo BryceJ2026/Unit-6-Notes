87fc94f5aaf80e46482f45152db850bf7fdb88bb
Bryce? <bryce.jones2026@gbwl.org>
2025-01-16 13:55:24 +0000

    ARRAY?

--- Main.java
+++ Main.java
@@ -1,6 +1,22 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      
+      // ARRAY(list) is a DATA STRUCTURE(collection)
+      //stores multiple values in one named variable
+
+      //DECLARE arrays using square brackets
+      // one array can hold items of ONE DATA TYPE
+      int[] luckyNumbers; // just creates a named REFERENCE, no values set
+      double[] mileTimes;
+      boolean[]trueOrFalse;
+      //Arrays can also store OBJECTS (reference types)
+      String[] spiritAnimal;
+
+      //TWO OPTIONS to CREATE the array in memory
+      // 1. Use the keyword NEW to set up an empty array
+      //Need to tell Java HOW MANY SPACES to allocate
+      luckyNumbers = new int[6]; // already declared earlier
+      int[] moreLuckyNumbers = new int[8]; //DECLARE + CREATE in one line
    }
 }
