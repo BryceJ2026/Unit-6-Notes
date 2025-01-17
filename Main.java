cf99b2f92b859157ae817518394caf452f0dfb30
Bryce? <bryce.jones2026@gbwl.org>
2025-01-17 14:42:28 +0000

    AAAAAAAAA

--- Main.java
+++ Main.java
@@ -18,5 +18,18 @@ public class Main {
       //Need to tell Java HOW MANY SPACES to allocate
       luckyNumbers = new int[6]; // already declared earlier
       int[] moreLuckyNumbers = new int[8]; //DECLARE + CREATE in one line
+      double[] movieRatings = new double[10];
+      String[] faveAnimals = new String[8];
+      System.out.println(moreLuckyNumbers);// Just prints location in memory
+
+      // 2. Use an INITIALIZER LIST to set array value
+      int[] lusckiestNumber = { 13, 13, 10, 9, 5, 20, 37, 7};
+      double[] currentCash = {1.0, 3.25, 15.0, 21.0};
+      String[] bestClass = {"Maia", "Alex", "Zoie", "Paige", "Natalie", "Bryce", "Finny", "Jackson"};
+      System.out.println(lusckiestNumber[0]); // first item is at index 0
+      System.out.println(bestClass[5]); //prints Bryce
+      
+
+
    }
 }
